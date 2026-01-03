2a582bb3a195e64d51de7d4045eb57ea39db681c
Luiz Fernando Torres <149012701+littlenando@users.noreply.github.com>
2026-01-03 17:06:07 -0300

    Initialize account variables and fix typos
    Added variables for saldo, depo, and saque. Fixed typo in completion message and improved saldo checks in tela_inicial function.

--- CODE.py
+++ CODE.py
@@ -1,5 +1,9 @@
 from sys import exit
 
+saldo = 0.0
+depo  = 0
+saque = 0
+
 def bemvindo():
     print("Bem vindo ao Banco Caixa Universal")
     resposta_1 = input("Você já tem conta com a gente? ").lower().strip()
@@ -26,7 +30,7 @@ def cadastro():
     email    = input("Qual sera seu endereço de e-mail? ")
     telefone = input("Telefone para contato: ")
     senha    = input("vamos criar uma senha de acesso: ")
-    print(f"Tudo pronto, seu Cadastro está comcluído!")
+    print(f"Tudo pronto, seu Cadastro está concluído!")
 
 def tela_inicial():
     print("1 -- Ver Saldo")
@@ -34,7 +38,18 @@ def tela_inicial():
     print("3 -- Fazer Depósito")
     print("4 -- Saque")
     escolha = input("Escolha qual opção quer fazer ")
-    #if escolha == "1": 
+    if escolha == "1":
+        if saldo <= 0:
+            print(f"O seu saldo é de R${saldo}")
+            tela_inicial()
+    elif escolha == "2":
+        if saldo <= 0:
+            print("Você não pode fazer transferência, pois não tem saldo")
+            tela_inicial()
+        else:
+            print("Quanto gostaria transferir?")
+
+        
 
         
 
