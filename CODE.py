c92a16cb036a2b81018a73bd8abdfea5ebd5c01e
Luiz Fernando Torres <149012701+littlenando@users.noreply.github.com>
2026-02-11 20:26:13 -0300

    Refactor CODE.py to include email and password

--- CODE.py
+++ CODE.py
@@ -1,11 +1,15 @@
+import os
 from sys import exit
 
-saldo = 0.0
-depo  = 0
-saque = 0
+saldo = 2.0
+email = "usuario@gmail.com"
+senha = "1234" 
+
+def limpar_tela():
+    os.system("cls" if os.name == "nt" else "clear")
 
 def bemvindo():
-    print("Bem vindo ao Banco Caixa Universal")
+    print("Bem-vindo ao Banco Caixa Universal")
     resposta_1 = input("Você já tem conta com a gente? ").lower().strip()
     if resposta_1 in ["nao", "n", "não"]:
         resposta_2 = input("Gostaria de fazer agora? ").lower().strip()
@@ -20,10 +24,20 @@ def bemvindo():
 
     else:
         print("Ótimo! Vamos acessar sua conta.")
-        tela_inicial()
+        resposta_3 = input("Email: ")
+        resposta_4 = input("Senha: ")
+        if resposta_3 == email and resposta_4 == senha:
+            limpar_tela()
+            print("Bem-vindo de volta, como podemos ajudar?")
+            tela_inicial()
+        else:
+            limpar_tela()
+            print("Email ou senha incorretos, tente novamente!")
+            bemvindo()
         
 
 def cadastro():
+    global email, senha
     print("Que bom que quer se juntar a nós!")
     nome     = input("Qual seu nome completo? ")
     cpf      = input("Qual o seu CPF? ")
@@ -31,29 +45,26 @@ def cadastro():
     telefone = input("Telefone para contato: ")
     senha    = input("vamos criar uma senha de acesso: ")
     print(f"Tudo pronto, seu Cadastro está concluído!")
+    limpar_tela()
 
 def tela_inicial():
-    print("1 -- Ver Saldo")
-    print("2 -- Fazer Transferencia")
-    print("3 -- Fazer Depósito")
+    print("1 -- Saldo")
+    print("2 -- Transferencia")
+    print("3 -- Depósito")
     print("4 -- Saque")
-    escolha = input("Escolha qual opção quer fazer ")
+    print("5 -- Configuração")
+    escolha = input("")
     if escolha == "1":
-        if saldo <= 0:
-            print(f"O seu saldo é de R${saldo}")
-            tela_inicial()
+        limpar_tela()
+        print(f"O seu saldo é de R${saldo:.2f}")
+        tela_inicial()
     elif escolha == "2":
         if saldo <= 0:
+            limpar_tela()
             print("Você não pode fazer transferência, pois não tem saldo")
             tela_inicial()
         else:
-            print("Quanto gostaria transferir?")
-
-        
-
-        
-
-
-
+            limpar_tela()
+            print("Qual o valor?")
 
 bemvindo()
